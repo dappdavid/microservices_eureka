ebe0b5e220ff7ab7639771166928a93b741eb87a
dappdavid <dappdavid99@gmail.com>
2022-10-03 16:24:09 +0530

    modified

---
+++ src/test/java/com/microservices_eureka/MicroservicesEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
